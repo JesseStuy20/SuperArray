36db5120baed35354800a974a293adc7dddc719e
JesseStuy20 <jxie20@stuy.edu>
2020-11-09 17:02:47 -0500

    Added zip method and test cases

--- Demo.java
+++ Demo.java
@@ -1,8 +1,8 @@
 public class Demo{
 
   public static void removeDuplicates(SuperArray s){
-	  for (int i=0; i<s.size(); i++){
-		  if (i != s.indexOf(s.get(i))){
+	  for (int i=0; i<s.size(); i++) {
+		  if (i != s.indexOf(s.get(i))) {
 			  s.remove(i);
 			  i--;
 		  }
@@ -20,16 +20,30 @@ public class Demo{
     return result ;
   }
 
+  public static SuperArray zip(SuperArray a, SuperArray b) {
+    int length = 0 ;
+    SuperArray result = new SuperArray() ;
+    if (a.size() >= b.size()) length = a.size() ;
+    else length = b.size() ;
+    for (int i=0; i<length; i++) {
+      if (i<a.size()) result.add(a.get(i)) ;
+      if (i<b.size()) result.add(b.get(i)) ;
+    }
+    return result ;
+  }
+
   public static void main(String[]args){
-    SuperArray words = new SuperArray();
+    SuperArray words = new SuperArray() ;
     //grouped to save vertical space
     words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
     words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
     words.add("una");    words.add("ebi");     words.add("toro");
 
-    System.out.println(words);
-    removeDuplicates(words);
-    System.out.println(words);
+    System.out.println(words) ;
+    removeDuplicates(words) ;
+    System.out.println(words) ;
+    SuperArray dupe = words ;
+    System.out.println(zip(words,dupe)) ;
 
   }
 }
